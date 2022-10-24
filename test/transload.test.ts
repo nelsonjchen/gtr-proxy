4878785ebdb30d899df6cfe9b22a7eeba72a35be
Nelson Chen <crazysim@gmail.com>
2022-10-23 19:26:06 -0700

    skaffold for future testing

---
+++ test/transload.test.ts
@@ -0,0 +1,7 @@
+describe('transload', () => {
+  test('can be done with an API of a readable stream from one URL to another', async () => {
+    const source_url = new URL('http://iscaliforniaonfire.com/')
+  
+  })
+
+})
